1763b0a62687dd8c81ef99c78e9a12b3e0ea29fd
Osama <osama54arra@gmail.com>
2025-08-19 11:05:06 +0300

    1st commet FS 12

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+// window.alert("in the name of Allah");
+// document.getElementById('wwe').innerHTML="hello";
+// var osama='osama54arra';
+// // window.console.log(typeof(osama));
+// var osama1=1.22;v 
+// var sam=osama1;
+// // window.console.log(sam);
+// window.console.log("3"*4);
+// var a=Number(window.prompt("your first Numper"));
+// var b=Number(window.prompt("Enter ur secund #"));
+// window.console.log(a);
+// window.console.log(b);
+// window.console.log(a+b); 
+var passDB="12"
+var GPA=3.000;
+var emailDB="osama54arra@gmail.com"
+var email = window.prompt("Enter your Email");
+var passwoed = window.prompt("Enter your password");
+var msg=`welcom ya ${passDB} hello`;
+window.console.log(msg);
+document.ri
+// window.console.log(passwoed);
+if(emailDB==email){
+    if(passwoed == passDB)
+    {   window.console.log("welcome");
+        document.getElementById("table1").innerHTML="<tr><td>1</td><td>welcome Osama</td><td>osama54arra@gmail.com</td></tr>";
+    } 
+    else{
+        window.console.log("Invalid Password")
+        document.getElementById("table1").innerHTML="<tr><td></td><td>Invalid Password</td><td></td></tr>";
+    }
+}
+else{
+    window.console.log("خطأ حاول مرة اخرى")
+    document.getElementById("table1").innerHTML="<tr><td></td><td>خطأ في الاجابة</td><td></td></tr>";
+}
+var user = {
+    name:"osama",
+    age:20,
+    gender:'mail',
+    dep:{
+        name:'it',
+        number:54,
+    }
+}
+
+window.console.log(user['name'])
+window.console.log(user.name)
+window.console.log(user)
+ user.salary=2500;
+ console.log(user.salary)
+window.console.log(user['salary'])nnerHTML=msg;
+window.console.log(user)
+window.console.log(user.dep)
+
+kye=[
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    }
+];   
